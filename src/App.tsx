653e66c8ae3ff5dacab800feb8bc2e462f4b01e6
Kristaps-M <kristapsmitins@gmail.com>
2025-02-08 17:10:50 +0200

    init, add- A.Details, urls, IAddress, get-address/es

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
+import "./App.css";
+import axios from "axios";
+import AddressDetail from "./pages/AddressDetail";
+import { IAddress } from "./Interfaces/IAddress";
+import { baseAddressesAPI } from "./Urls/addressesApiUrl";
 
 function App() {
+  const [addresses, setAddresses] = useState<IAddress[]>([]);
+
+  useEffect(() => {
+    // Make a request for a user with a given ID
+    axios
+      .get(baseAddressesAPI)
+      .then(function (response) {
+        // handle success
+        const theA = response.data.addresses;
+        setAddresses(theA);
+        console.log(theA);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <div>--- Addresses ultimate Table! ---</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddressTable addresses={addresses} />} />
+        <Route path="/address/:addressId" element={<AddressDetail />} />
+        {/* {addressTable(addresses)} */}
+      </Routes>
+    </BrowserRouter>
+    // </div>
   );
 }
 
 export default App;
+
+function AddressTable({ addresses }: { addresses: IAddress[] }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Address</th>
+          <th>Country</th>
+          <th>Zip</th>
+        </tr>
+      </thead>
+      <tbody>
+        {addresses.map((address) => (
+          <AddressTableRow key={address.id} address={address} />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function AddressTableRow({ address }: { address: IAddress }) {
+  return (
+    <tr>
+      <td>
+        <Link to={`/address/${address.id}`}>{address?.id}</Link>
+      </td>
+      <td>{address?.address}</td>
+      <td>{address?.country}</td>
+      <td>{address?.zip}</td>
+    </tr>
+  );
+}
---
+++ src/pages/AddressDetail.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IAddress } from "../Interfaces/IAddress";
+import { baseAddressesAPI } from "../Urls/addressesApiUrl";
+
+export default function AddressDetail() {
+  const { addressId } = useParams();
+  const [address, setAddress] = useState<IAddress>();
+  useEffect(() => {
+    // Make a request for a user with a given ID
+    axios
+      .get(`${baseAddressesAPI}/address/${addressId}`)
+      .then(function (response) {
+        // handle success
+        const theA = response.data.address;
+        setAddress(theA);
+        console.log(theA);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, [addressId]);
+
+  return (
+    <div style={{ marginLeft: "8rem" }}>
+      <h2>Address Details</h2>
+      <p>Address ID: {addressId}</p>
+      <p>Address: {address?.address}</p>
+      <p>Country: {address?.country}</p>
+      <p>Zip: {address?.zip}</p>
+    </div>
+  );
+}
